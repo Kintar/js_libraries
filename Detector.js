09d122a57ca9b3de5a273156b38427a7e9e58cb8
Alec Lanter <kintar1900@gmail.com>
2012-08-23 17:19:17 -0500

    Added new support files

---
+++ Detector.js
@@ -0,0 +1,13 @@
+var Detector = {
+    canvas: !! window.CanvasRenderingContext2D,
+    webgl: (function() {
+        try {
+            return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl');
+        }
+        catch (e) {
+            return false;
+        }
+    })(),
+    workers: !! window.Worker,
+    fileapi: window.File && window.FileReader && window.FileList && window.Blob
+};
